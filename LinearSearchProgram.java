953428a5468cc1f8a3dca6a05756a52e77021e6a
sai232-cse <saimahitha232@gmail.com>
2025-05-18 20:21:08 +0530

    Update LinearSearchProgram.java

--- LinearSearchProgram.java
+++ LinearSearchProgram.java
@@ -7,13 +7,13 @@ public class LinearSearchProgram{
         Scanner snr=new Scanner(System.in);
         System.out.println("enter the size of array");
         int size=snr.nextInt();
-	    int array[]=new int[size];
+	int array[]=new int[size];
         System.out.println("enter the elements");
         for(int i=0;i<size;i++){
             array[i]=snr.nextInt();
         }
 
-	    System.out.println("enter the value to be searched");
+	System.out.println("enter the value to be searched");
         int element=snr.nextInt();
 
         boolean found=false;
