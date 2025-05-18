9c6f6f05393da11cc2e043200157e91b36a09000
sai232-cse <saimahitha232@gmail.com>
2025-05-18 20:48:27 +0530

    Update LinearSearchProgram.java

--- LinearSearchProgram.java
+++ LinearSearchProgram.java
@@ -9,18 +9,17 @@ public class LinearSearchProgram{
         int size=snr.nextInt();
 	int array[]=new int[size];
         System.out.println("enter the elements");
+	    
         for(int i=0;i<size;i++){
             array[i]=snr.nextInt();
         }
 
 	System.out.println("enter the value to be searched");
         int element=snr.nextInt();
-
         boolean found=false;
+	    
         for(int i=0;i<size;i++){
-
             if(array[i]==element){
-
                 System.out.println("the value is found at index "+i);
                 found=true;
             }
