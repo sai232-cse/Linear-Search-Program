9a9d2563d9528e64c40994235bb6b44109b926a0
sai232-cse <saimahitha232@gmail.com>
2025-05-18 20:20:21 +0530

    Add files via upload

---
+++ LinearSearchProgram.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class LinearSearchProgram{
+
+    public static void main(String args[]){
+
+        Scanner snr=new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int size=snr.nextInt();
+	    int array[]=new int[size];
+        System.out.println("enter the elements");
+        for(int i=0;i<size;i++){
+            array[i]=snr.nextInt();
+        }
+
+	    System.out.println("enter the value to be searched");
+        int element=snr.nextInt();
+
+        boolean found=false;
+        for(int i=0;i<size;i++){
+
+            if(array[i]==element){
+
+                System.out.println("the value is found at index "+i);
+                found=true;
+            }
+        }
+
+        if(!found){
+            System.out.println("not found");
+        }
+    }
+}
